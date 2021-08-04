8c1391035d91334cc1ead2f908e8743e6d84e772
Luis Denis <denisvarela@gmail.com>
2021-08-04 14:13:42 -0400

    comit init project..

---
+++ example/src/test/java/cl/globallogic/bci/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.globallogic.bci.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
